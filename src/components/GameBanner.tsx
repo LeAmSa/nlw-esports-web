2b366d47318b862d25fbc54b9fa6d8a50aa99b74
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-17 21:51:39 -0300

    Add mobile responsive styles

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -3,22 +3,23 @@ interface GameBannerProps {
   title: string;
   adsCount: number;
   className?: string;
+  onClick: () => void;
 }
 
 function GameBanner(props: GameBannerProps) {
   return (
-    <a
-      href=""
-      className={`relative rounded-lg overflow-hidden ${props.className}`}
+    <div
+      className={`relative rounded-lg overflow-hidden cursor-pointer z-0 ${props.className}`}
+      onClick={props.onClick}
     >
       <img src={props.bannerUrl} alt="" />
-      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+      <div className="w-full pt-16 pb-4 px-2 md:px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{props.title}</strong>
         <span className="text-zinc-300 text-sm block">
           {props.adsCount} anúncio(s)
         </span>
       </div>
-    </a>
+    </div>
   );
 }
 

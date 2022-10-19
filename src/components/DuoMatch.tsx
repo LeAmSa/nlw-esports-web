51a4a1a3afa5132dd9d2ab0b4125fc21027faee4
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-19 13:24:42 -0300

    Add copy to clipboard

--- src/components/DuoMatch.tsx
+++ src/components/DuoMatch.tsx
@@ -1,16 +1,24 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X, CheckCircle, CopySimple } from "phosphor-react";
+import { X, Check, CheckCircle, CopySimple } from "phosphor-react";
+import { useState } from "react";
+
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 interface DuoMatchProps {
   discord: string;
 }
 
 export function DuoMatch({ discord }: DuoMatchProps) {
+  const [discordCopied, setDiscordCopied] = useState(false);
+
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed z-50" />
 
-      <Dialog.Content className="fixed p-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-[310px] bg-[#2A2634] rounded-lg flex flex-col items-center justify-center shadow-lg">
+      <Dialog.Content
+        className="fixed p-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-[310px] bg-[#2A2634] rounded-lg flex flex-col items-center justify-center shadow-lg"
+        onCloseAutoFocus={() => setDiscordCopied(false)}
+      >
         <Dialog.Close className="text-gray-400 hover:text-white transition-colors self-end">
           <X size={20} weight="bold" />
         </Dialog.Close>
@@ -24,11 +32,21 @@ export function DuoMatch({ discord }: DuoMatchProps) {
 
           <div className="w-full flex flex-col gap-2 items-center">
             <span className="text-white font-bold">Adicione no discord</span>
-            <button className="w-full flex items-center justify-between bg-[#121214] px-2 py-4 text-white rounded-lg mb-4 shadow-lg">
-              <div></div>
-              {discord}
-              <CopySimple size={20} color="#9ca3af" />
-            </button>
+
+            <CopyToClipboard
+              text={discord}
+              onCopy={() => setDiscordCopied(true)}
+            >
+              <button className="w-full flex items-center justify-between bg-[#121214] px-2 py-4 text-white rounded-lg mb-4 shadow-lg">
+                <div></div>
+                {discord}
+                {!discordCopied ? (
+                  <CopySimple size={20} color="#9ca3af" />
+                ) : (
+                  <Check size={20} color="#22c55e" weight="bold" />
+                )}
+              </button>
+            </CopyToClipboard>
           </div>
         </main>
       </Dialog.Content>

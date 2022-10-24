1a133181628ab3be1f05a9d33be2c9fa2d703e07
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-24 15:11:40 -0300

    Add gamebanner hover effect

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -8,16 +8,18 @@ interface GameBannerProps {
 
 function GameBanner(props: GameBannerProps) {
   return (
-    <div
-      className={`relative rounded-lg overflow-hidden cursor-pointer z-0 ${props.className}`}
-      onClick={props.onClick}
-    >
-      <img src={props.bannerUrl} alt="" />
-      <div className="w-full pt-16 pb-4 px-2 md:px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-        <strong className="font-bold text-white block">{props.title}</strong>
-        <span className="text-zinc-300 text-sm block">
-          {props.adsCount} anúncio(s)
-        </span>
+    <div className="rounded-lg bg-violet-500 -z-10">
+      <div
+        className={`relative rounded-lg overflow-hidden cursor-pointer z-0 ${props.className} hover:translate-x-2 hover:-translate-y-2 transition-transform`}
+        onClick={props.onClick}
+      >
+        <img src={props.bannerUrl} alt="Banner do game" />
+        <div className="w-full pt-16 pb-4 px-2 md:px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">{props.title}</strong>
+          <span className="text-zinc-300 text-sm block">
+            {props.adsCount} anúncio(s)
+          </span>
+        </div>
       </div>
     </div>
   );

c6cc2de73b41b2b4e6fa20e6019d7423be1c91a3
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-24 15:12:31 -0300

    Fix nlw-gradient

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-    './index.html'
-  ],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     fontFamily: {
-      sans: ['Inter', 'sans-serif']
+      sans: ["Inter", "sans-serif"],
     },
     extend: {
-      
       backgroundImage: {
         galaxy: "url('/bg-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
-        'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.08%)'
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 30.33%, #43E7AD 50.55%, #E1D55D 100%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.08%)",
       },
     },
   },
   plugins: [],
-}
+};
